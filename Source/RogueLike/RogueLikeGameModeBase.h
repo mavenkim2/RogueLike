a6f2d3c95aafe190c6fc5618f0ccd8a25edaa624
mavenkim2 <55301281+mavenkim2@users.noreply.github.com>
2023-06-26 19:09:16 -0400

    init

---
+++ Source/RogueLike/RogueLikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RogueLikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKE_API ARogueLikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
